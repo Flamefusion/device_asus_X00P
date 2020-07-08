aca9fe8921290c2d1b7965948b8562641e4a42c5
Artem Borisov <dedsa2002@gmail.com>
2020-07-08 09:54:30 +0530

    X00P: disable cds module
    * fixes green artifacts in camera

--- prop.mk
+++ prop.mk
@@ -71,7 +71,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.HAL3.enabled=1 \
     persist.camera.is_type=1 \
     persist.ts.rtmakeup=true \
-    vidc.enc.dcvs.extra-buff-count=2
+    vidc.enc.dcvs.extra-buff-count=2 \
+    persist.camera.CDS=off
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
